b0d9b4555b3c1c1763305d26a52f263f1d675e25
NathanMcCormack <G00414780@atu.ie>
2023-10-30 10:37:26 +0000

    Added a new instance of Student.

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -3,9 +3,16 @@ package ie.atu;
 public class StudentApp {
     public static void main(String[] args) {
         Student student1 = new Student();
+        Student student3 = new Student("", "", "");
 
+        System.out.println("Enter details for first student: ");
         student1.getStudentInfo();
+
+        System.out.println("Enter details of third student: ");
+        student3.getStudentInfo();
+
         student1.displayInfo();
+        student3.displayInfo();
     }
 }
 
